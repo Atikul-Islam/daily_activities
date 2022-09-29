5a540b74cdedd12ca6ac47450da7b8d291999302
Atikul-Islam <atikmina25@gmail.com>
2022-09-29 22:44:38 +0600

    added blog

--- src/components/Exercises/Exercises.js
+++ src/components/Exercises/Exercises.js
@@ -36,11 +36,28 @@ const Exercises = () => {
                 <div className='blog'>
                     <h1>Blog</h1>
                    <h3>How react works?</h3>
-                   <p>answer:</p>
+                   <p>While building client-side apps, a team of Facebook developers realized that the DOM is slow (The Document Object Model (DOM) is an application programming interface (API) for HTML and XML documents. It defines the logical structure of documents and the way a document is accessed and manipulated.). So, to make it faster, React implements a virtual DOM that is basically a DOM tree representation in JavaScript. So when it needs to read or write to the DOM, it will use the virtual representation of it. Then the virtual DOM will try to find the most efficient way to update the browser’s DOM.
+                    Unlike browser DOM elements, React elements are plain objects and are cheap to create. React DOM takes care of updating the DOM to match the React elements. The reason for this is that JavaScript is very fast and it’s worth keeping a DOM tree in it to speed up its manipulation.
+                    Although React was conceived to be used in the browser, because of its design it can also be used in the server with Node.js.</p>
                    <h3>What is the difference between props and state?</h3>
-                   <p>answer:</p>
+                   <p>Props: <br />
+                    The Data is passed from one component to another. <br />
+                    It is Immutable (cannot be modified).	<br />
+                    Props can be used with state and functional components. <br />	
+                    Props are read-only. <br /> <br />
+
+                    State: <br />
+                    The Data is passed within the component only. <br />
+                    It is Mutable ( can be modified). <br />
+                    State can be used only with the state components/class component (Before 16.0). <br />
+                    State is both read and write. </p>
                    <h3>What is the uses of useEffect without data load?</h3>
-                   <p>answer</p>
+                   <p>Running once on mount: fetch API data <br />
+                    Running on state change: validating input field <br />
+                     Running on state change: live filtering <br />
+                    Running on state change: trigger animation on new array value <br />
+                    Running on props change: update paragraph list on fetched API data update <br />
+                    Running on props change: updating fetched API data to get BTC updated price <br /> </p>
                 </div>
             </div>
             <div className='profile-container'>
